2130eb45d34e568347e404e036ae187018b7089d
tariba <tariqxa@gmail.com>
2024-09-18 14:06:11 +0100

    added the code

---
+++ application-project/prompts/code.py
@@ -0,0 +1,31 @@
+#This displays the total shop inventory
+shop_inventory = {"Books": ['Malcolm X', 'Learn how to code for Dummies,' 'Life of Pi'],
+                  "Drinks": ['Latte', 'Coffee', 'Tea', 'Juice'],
+                   "Food": ["Sandwich", "Pastry", "Salad", "Cookie"] }
+ 
+ 
+#This function represents the main menu screen
+def customer_menu() :
+     print('1. Books')
+     print('2. Food')
+     print('3. Drink')
+     print('3. Exit')
+ 
+def employee_menu():
+     return print('testing')
+ 
+def main_menu():
+     print('Welcome to Espresso Yourself.\nPlease select one of the following')
+     print('1. Customer')
+     print('2. Employee')
+     print('3. Exit')
+     user_input=input('Enter 1 for Customer and 2 for Employee:')
+ 
+     if user_input == '1':
+         customer_menu()
+     elif user_input =='2':
+         employee_menu()
+     else:
+         print('Goodbye')
+ 
+main_menu()
