db6d88fe6e6ea42122c148a5658434068a9dae83
tariba <tariqxa@gmail.com>
2024-09-18 14:29:57 +0100

    created nested dictionary for inventories

--- application-project/prompts/code.py
+++ application-project/prompts/code.py
@@ -1,9 +1,18 @@
 #This displays the total shop inventory
 shop_inventory = {"Books": {"1":"Malcolm X",
                              "2": 'Learn how to code for Dummies',
-                               "3": 'Life of Pi',}
-                  "Drinks": ['Latte', 'Coffee', 'Tea', 'Juice'],
-                   "Food": ["Sandwich", "Pastry", "Salad", "Cookie"] }
+                               "3": 'Life of Pi'},
+
+                  "Drinks": {'1': 'Latte', 
+                             '2': 'Coffee', 
+                             '3': 'Tea', 
+                             '4': 'Juice'},
+
+                   "Food": {'1': "Sandwich", 
+                            '2': "Pastry", 
+                            '3': "Salad", 
+                            '4': "Cookie"}
+                   }
  
  
 #This function represents the main menu screen
@@ -14,8 +23,14 @@ def customer_menu() :
      print('3. Exit')
 
      user_input = input("Please could you type")
+     
+     
+
  
 def employee_menu():
+     print('1. View Inventory')
+     print('2. Remove Items from Inventory')
+     print('3. Add Items to the Inventory')
      return print('testing')
  
 def main_menu():
